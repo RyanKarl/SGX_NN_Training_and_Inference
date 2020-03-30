fee4def046b34bbf3db41c9f92083fe9eeb28756
RyanKarl <rkarl@nd.edu>
2020-03-30 13:33:09 -0400

    Masking Test

--- examples/mnist/enclave_masking.c
+++ examples/mnist/enclave_masking.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 
+/*
 int maxpool(int *matrix, int m_height_width, int k_height_width, int stride)
 {
     int *max_matrix = (int *)malloc((m_height_width - k_height_width) / stride * sizeof(int));
@@ -18,14 +19,26 @@ int maxpool(int *matrix, int m_height_width, int k_height_width, int stride)
 
         }
     return max;
+}*/
+
+/*
+void mask(int*  matrix, int m_height, int m_width, int r)
+{
+    int i, j, count = 0;
+        for (i = 0; i <  m_height; i++)
+            for (j = 0; j < m_width; j++)
+                *(matrix + i*m_width + j) = *(matrix + i*m_width + j) + r;
 }
+*/
+
 
 int main() 
 { 
 
     srand(time(NULL));   // Initialization, should only be called once.
-    int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+    int r = rand() % 10;      // Returns a pseudo-random integer between 0 and RAND_MAX.
     
+
     int h = 4, w = 4;
     //matrix is primary input 
     int *matrix = (int *)malloc(h * w * sizeof(int)); 
@@ -33,10 +46,23 @@ int main()
     int i, j, count = 0; 
     for (i = 0; i <  h; i++) 
         for (j = 0; j < w; j++) 
-            *(matrix + i*w + j) = ++count; 
-       
+            *(matrix + i*w + j) = count++; 
+
+    //printf("Test");
+    for (i = 0; i <  h; i++)
+        for (j = 0; j < w; j++)
+            printf("%d ", *(matrix + i*w + j));
+
     
-    maxpool(*matrix, w, 2, 1);
+    for (i = 0; i <  h; i++)
+        for (j = 0; j < w; j++)
+            *(matrix + i*w + j) = *(matrix + i*w + j) + r;
+
+    for (i = 0; i <  h; i++)
+        for (j = 0; j < w; j++)
+            printf("%d ", *(matrix + i*w + j));
+      
+      //maxpool(*matrix, h, 2, 1);
     
 
       //ret = tanh(x);      
