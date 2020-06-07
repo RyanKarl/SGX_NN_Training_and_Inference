a78073b9d4570ab1758c29e16522ff0234dfaaeb
Mark Horeni <mhoreni@nd.edu>
2020-06-07 11:26:28 -0400

    passing the baton straight back to jon

--- examples/mnist/caster.py
+++ examples/mnist/caster.py
@@ -9,13 +9,19 @@ spc.start(verbose=3)
 
 f = open("caster.dat", "wb")
 
+
 layers = 5
+
+activations = [None] * layers
+weights = [None] * layers
+outputs = [None] * layers
 
 for i in range(layers):
     print("GPU on layer " + str(i+1))
     if spc.good():
         a = spc.read_matrix_from_enclave()
         a = a.astype(np.float32)
+        activations[i] = a
         print(a.shape)
     else:
         sys.exit(1)
@@ -23,13 +29,16 @@ for i in range(layers):
     if spc.good():
         b = spc.read_matrix_from_enclave()
         b = b.astype(np.float32)
+        weights[i] = b
         print(b.shape)
     else:
         sys.exit(1)
 
     if spc.good():
-        spc.send_to_enclave((a @ b))
-        outdata = spc.validate_one_matrix((a @ b))
+        c = (a @ b)
+        spc.send_to_enclave(c)
+        outputs[i] = c
+        outdata = spc.validate_one_matrix(c)
         f.write(outdata[0])
         f.write(outdata[1])
         #spc.send_to_enclave((a @ b.t()))
@@ -39,29 +48,36 @@ for i in range(layers):
         
 print("GPU starting backprop")        
         
-for i in range(layers):     
-  if spc.good():
-    x = spc.read_matrix_from_enclave()
-    assert(x.dtype == np.float32)
-  if spc.good():
-    y = spc.read_matrix_from_enclave()  
-  if spc.good():
-    z = spc.read_matrix_from_enclave()    
-    
-  xy = x @ y
-  xz = x @ z
+for i in range(layers)[::-1]: 
 
   if spc.good():
-    spc.send_to_enclave(xy)
-    outdata = spc.validate_one_matrix(xy)
+    spc.send_to_enclave(outputs[i])
+
+  if spc.good():
+    grad_output = spc.read_matrix_from_enclave()
+    assert(grad_ouput.dtype == np.float32)
+
+
+  d = grad_output @ weights[i].transpose()
+  e = grad_output.transpose() @ actvations[i]
+
+  if spc.good():
+    spc.send_to_enclave(d)
+    outdata = spc.validate_one_matrix(d)
     f.write(outdata[0])
     f.write(outdata[1])  
+
   if spc.good():
-    spc.send_to_enclave(xz)
-    outdata = spc.validate_one_matrix(xz)
+    spc.send_to_enclave(e)
+    outdata = spc.validate_one_matrix(e)
     f.write(outdata[0])
     f.write(outdata[1])    
-        
+    
+  if spc.good():
+    spc.send_to_enclave(activations[i])
+  if spc.good():
+    spc.send_to_enclave(weights[i])
+
         
         
     
