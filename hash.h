3d287cfe4c70be6757d34a6a153fdb37ced9447d
Jonathan Takeshita <jtakeshi@nd.edu>
2020-06-30 15:16:42 -0400

    Added basic hash function

---
+++ hash.h
@@ -0,0 +1,14 @@
+#ifndef HASH_H
+#define HASH_H
+#include <functional>
+
+size_t float_hash(float * data, size_t num_floats){
+  static std::hash<float> h;
+  size_t ret = 0;
+  for(size_t i = 0; i < num_floats; i++){
+    ret ^= (h(data[i]) << 1);
+  }
+  return ret;
+}
+
+#endif
